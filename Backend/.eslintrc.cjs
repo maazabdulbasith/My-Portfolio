e3b41cb9bba8c0ccd35ff1ac11a58bbdf87f4859
Maaz Abdul Basith <maazabdulbasith@gmail.com>
2025-04-30 18:25:21 +0530

    Fix ESLint config for backend

--- Backend/.eslintrc.cjs
+++ Backend/.eslintrc.cjs
@@ -1,25 +1,28 @@
 module.exports = {
     extends: [
         "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:jsx-a11y/recommended",
         "plugin:import/errors",
         "plugin:import/warnings",
     ],
     parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
+        ecmaVersion: 2021, // Specify ECMAScript version
+        sourceType: "module", // Allow import/export syntax
+        ecmaFeatures: {
+            jsx: true,
+        },
     },
-    plugins: ["import"],
+    plugins: ["react", "jsx-a11y", "import"],
     rules: {},
     ignorePatterns: ["node_modules/", "build/"],
     settings: {
-        "import/resolver": {
-            node: {
-                paths: ["."],
-                extensions: [".js", ".jsx"],
-            },
+        react: {
+            version: "detect",
         },
     },
     env: {
+        browser: true,
         node: true,
         es2021: true,
     },
